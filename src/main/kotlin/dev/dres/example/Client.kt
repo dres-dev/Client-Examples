9764e4427c2badd49db208d15bd4288c13f756ed
Luca Rossetto <rossetto@ifi.uzh.ch>
2020-12-22 14:36:02 +0100

    Added example call for submission

--- src/main/kotlin/dev/dres/example/Client.kt
+++ src/main/kotlin/dev/dres/example/Client.kt
@@ -1,5 +1,6 @@
 package dev.dres.example
 
+import dev.dres.client.SubmissionApi
 import dev.dres.client.UserApi
 import org.openapitools.client.infrastructure.ClientException
 import org.openapitools.client.models.LoginRequest
@@ -12,6 +13,9 @@ object Client {
 
         //initialize user api client
         val userApi = UserApi(Settings.basePath)
+
+        //initialize user api client
+        val submissionApi = SubmissionApi(Settings.basePath)
 
         println("Trying to log in to '${Settings.basePath}' with user '${Settings.user}'")
 
@@ -40,12 +44,30 @@ object Client {
         //store session token for future requests
         val sessionId = login.sessionId!!
 
+        Thread.sleep(1000)
 
-        /*
+        val submissionResponse =
+                try {
+                    submissionApi.getSubmit(
+                            session = sessionId,
+                            collection = null, //does not usually need to be set
+                            item = "some_item_name", //item which is to be submitted
+                            frame = null, // for items with temporal components, such as video
+                            shot = null,  // only one of the time fields needs to be set.
+                            timecode = "00:00:10:00" //in this case, we use the timestamp in the form HH:MM:SS:FF
+                    )
+                } catch (clientException: ClientException) {
+                    when (clientException.statusCode) {
+                        401 -> System.err.println("There was an authentication error during the submission. Check the session id.")
+                        404 -> System.err.println("There is currently no active task which would accept submissions.")
+                        else -> System.err.println("Something unexpected went wrong during the submission: '${clientException.message}'.")
+                    }
+                    null
+                }
 
-        ... do other things ...
-
-         */
+        if (submissionResponse != null && submissionResponse.status) {
+            println("The submission was successfully sent to the server.")
+        }
 
         Thread.sleep(1000) //doing other things...
 
